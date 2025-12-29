aba3d076b7f3a9101ec6e64cffaf46fe09ec282e
ilyaskadjahou <ilyaskadjahou.cs@gmail.com>
2025-12-29 05:06:44 +0100

    Add admin configuration for Course, Lesson, and Question

---
+++ onlinecourse/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Course, Lesson, Question, Choice, Submission
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
